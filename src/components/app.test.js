9492181c3da189fd811b7a016b02ffe365d4c6d6
Jonathan KUMA <jonathan.kuma@gmail.com>
2018-07-31 00:15:42 +0200

    Fix up test for app component.

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxPromise from 'redux-promise'
+import App from './app'
+import reducers from '../reducers/index'
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App/>
+    </Provider>, div)
+  ReactDOM.unmountComponentAtNode(div)
+})

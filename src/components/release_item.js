780805218c85e12b3a509faad7ceda196aaef523
Jonathan KUMA <jonathan.kuma@gmail.com>
2018-08-01 14:32:58 +0200

    Bind a link to discogs release page on each item.

--- src/components/release_item.js
+++ src/components/release_item.js
@@ -5,15 +5,22 @@ class ReleaseItem extends Component {
 
   render () {
     let {title} = this.props.data
+    let {id} = this.props.data
     let {size} = this.props
     let image = this.props.data.cover_image
+
+    const url = `https://www.discogs.com/release/${id}`
 
     if ('small' === this.props.coverSize) {
       size = 150
       image = this.props.data.thumb
     }
 
-    return <img height={size} width={size} src={image} title={title} alt={title}/>
+    return (
+      <a href={url} target="_blank">
+        <img height={size} width={size} src={image} title={title} alt={title}/>
+      </a>
+    )
   }
 }
 
@@ -21,7 +28,7 @@ ReleaseItem.defaultProps = {
   size: 250
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {coverSize: state.coverSize}
 }
 

418608d38322ea28e60e95ec0792cbd5edfdd8d8
Jonathan KUMA <jonathan.kuma@gmail.com>
2018-12-09 00:20:06 +0100

    Fix up wrong import in DialogError component.

--- src/components/dialog_error.js
+++ src/components/dialog_error.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import connect from 'react-redux/es/connect/connect';
+import { connect } from 'react-redux';
 
 class DialogError extends Component {
   render() {
